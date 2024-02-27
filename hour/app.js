2e8262c1e3a2227f2f740cac421a8146f3223724
Rasul Jangirli <resulcahangirli99@gmail.com>
2024-02-28 01:55:07 +0400

    hour
    hour

---
+++ hour/app.js
@@ -0,0 +1,16 @@
+const houre = document.querySelector("#hour_p");
+const minute = document.querySelector("#minute_p");
+const second = document.querySelector("#second_p");
+
+function hourUI() {
+    let data = new Date();
+
+    houre.innerHTML = dabbleNumber(data.getHours());
+    minute.innerHTML = dabbleNumber(data.getMinutes());
+    second.innerHTML = dabbleNumber(data.getSeconds());
+}
+function dabbleNumber(x) {
+    return x < 10 ? `0${x}` : x;
+}
+hourUI();
+setInterval(hourUI, 1000)
